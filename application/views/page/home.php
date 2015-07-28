08a54686cef697e18948b17d2cecceb5be2df3a4
Lex Slaghuis <lex@wikiwise.nl>
2015-07-28 09:24:18 +0200

    Whatever
    Former-commit-id: 60dbb55c62c24186f55949fa3ab50c79550cec0c

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -9,6 +9,7 @@
 <p>De gehele database in het oorspronkelijke formaat te downloaden. Het is een gecomprimeerd json bestand dat bestaat uit ongeveer 
  4500 hashes met daarin de velden: 'name', 'set' (De instantie), en 'html'. Het html veld bevat de gehele gecrawlde pagina plus de oorspronkelijke http headers.</p>
 
- <a href="http://ors.openstate.eu/rechtspraak-2014-05-18.json.gz">rechtspraak-2014-05-18.json.gz</a>. 
+<p><a href="http://ors.openstate.eu/old-data/rechtspraak-2014-05-18.json.gz">rechtspraak-2014-05-18.json.gz</a>.</p>
+<p><a href="http://ors.openstate.eu/old-data/rechtspraak-2015-07-27.json.gz">rechtspraak-2015-07-27.json.gz</a>.</p>
 <p/>
 <p/>
