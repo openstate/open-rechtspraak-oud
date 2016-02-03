a2e7104339a452cb7085344ab7e75e7a160a6b9d
Lex Slaghuis <lex@wikiwise.nl>
2016-02-03 16:34:23 +0100

    whatever
    Former-commit-id: 1be4244eb7245a7f393244fe8df689dcf3623d82

--- application/controllers/relations.php
+++ application/controllers/relations.php
@@ -34,24 +34,29 @@ class Relations extends CI_Controller {
 
         // get the index
         $file = file_get_contents("rechtspraak-index.json");
-        $data['json'] = json_decode($file, true);
+        $json = json_decode($file, true);
 
+        // {"set":"Rechtbank Amsterdam","name":"mw. mr. J.F. Aalders ","file":"rechtspraak\/mw.+mr.+J.F.+Aalders+.txt"},
+        // -->
+        // {"set":"Rechtbank Amsterdam","name":"mw. mr. J.F. Aalders ","uri"},
+        $newjson = array();
+        foreach ($json as $element) {
+            $uri = site_url(array('relations', 'instantie', urlencode($element["set"]),
+                urlencode($element["name"]), "json"));
+
+            $newjson[] = array("set" => $element["set"], "name" => $element["name"], "uri" => $uri);
+        }
+//var_dump($newjson);
+//die();
         $this->output
                 ->set_content_type('application/json')
-                ->set_output(json_encode($data['json']));
+                ->set_output(json_encode($newjson));
     }
 
-    public function instantie($enc_set, $enc_name) {
-        // get ALL the fucking data
-        //  die("$enc_name");
-        if ($enc_name == "mw.+mr.+D.M.+Staal+") {
-            die("404");
-        }
-
+    public function instantie($enc_set, $enc_name, $option = "noJson") {
 
         $set = urldecode($enc_set);
         $name = urldecode($enc_name);
-
 
         $file = file_get_contents("rechtspraak-index.json");
         $json = json_decode($file, true);
@@ -64,15 +69,37 @@ class Relations extends CI_Controller {
         }
 
         if (!isset($data['json'])) {
-            error_log("ERRoR in RELATIONS SHOWING STUF TO CLIENTS");
+            error_log("Error");
             die("error in relations controller!");
         }
 
-        // render page
-        $this->load->library('parser');
-        $this->parser->parse('header', array('dummy' => 'dummy'));
-        $this->parser->parse('relation_view', $data); //, $json); //default view
-        $this->load->view('footer');
+        if ($option === "json") {
+            $json = $data['json'];
+
+            $doc = new DOMDocument();
+            @$doc->loadHTML($json['html']);
+            $xpath = new DOMXpath($doc);
+
+            $elements = $xpath->query("//div[@class='details']");
+            if ($elements->length == 1) {
+                $details = $doc->saveHTML($elements->item(0));
+            } else {
+                error_log("Page didn't contain div@[class=details]");
+                echo "<i>Foutmelding aan 112: HTML data bevat geen div@[class=details]</i>";
+            }
+
+            $newjson = array('name' => $json['name'], 'set' => $json['set'], 'details' => $details);
+            $this->output
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode($newjson));
+            return;
+        } else {
+            // render page
+            $this->load->library('parser');
+            $this->parser->parse('header', array('dummy' => 'dummy'));
+            $this->parser->parse('relation_view', $data); //, $json); //default view
+            $this->load->view('footer');
+        }
     }
 
 }
