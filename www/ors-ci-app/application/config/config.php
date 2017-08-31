937e71ebcdc0529aa97e9fe0c51e8558b089c63c
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-08-31 14:32:17 +0200

    Use HTTPS

--- www/ors-ci-app/application/config/config.php
+++ www/ors-ci-app/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ors.openstate.eu';
+$config['base_url']	= 'https://ors.openstate.eu';
 //$config['base_url']	= 'http://localhost';
 /*
 |--------------------------------------------------------------------------
