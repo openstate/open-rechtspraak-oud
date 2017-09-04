9e80f1fd14dfe0d720b62c7e9945cb4bc248dc09
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-09-04 17:30:37 +0200

    Use HTTPS when linking to openstate.eu

--- www/ors-ci-app/application/views/page/contact.php
+++ www/ors-ci-app/application/views/page/contact.php
@@ -1,4 +1,4 @@
 <p>
     <h3>Contact</h3></p>
-<p>Deze website is gerealiseerd door <a href='http://openstate.eu/'>Open State Foundation.</a></p> 
+<p>Deze website is gerealiseerd door <a href='https://openstate.eu/'>Open State Foundation.</a></p> 
 <p>Er worden geen contactgegevens van personen verstrekt aan derden.</p>
